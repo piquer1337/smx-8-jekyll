66ba56ca72110ab3cf8aa47cfb2e679a91f097af
David de Mingo <ddemingo@xtec.cat>
2022-10-17 06:34:37 +0000

    add init to  jekyll.sh

--- jekyll.sh
+++ jekyll.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-# init (TODO)
-touch Gemfile.lock
-chmod a+w Gemfile.lock
+init() {
+    touch Gemfile.lock
+    chmod a+w Gemfile.lock
 
-touch .jekyll-metadata
-chmod a+w .jekyll-metadata
+    touch .jekyll-metadata
+    chmod a+w .jekyll-metadata
+
+    docker-compose up
+}
+
+case $1 in 
+    init)
+        init
+    ;;
+    *)
+        echo "Usage: $0 init"
+    ;;
+esac
