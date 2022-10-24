b9df93fed26841d7a1a8565a5bf8c31d0f5bb3b3
Sergi <smpiquer50@gmail.com>
2022-10-24 07:22:03 +0000

    jekyll_mejorado

--- jekyll.sh
+++ jekyll.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
-run() {
-    touch Gemfile.lock
-    chmod a+w Gemfile.lock
+export JEKYLL_VERSION=3.8.6
 
-    touch .jekyll-metadata
-    chmod a+w .jekyll-metadata
+update() {
 
-    docker-compose up
+    docker run --rm \
+        --env JEKYLL_UID=$UID \
+        --env JEKYLL_GID=$UID \
+        --volume="$PWD:/srv/jekyll" \
+        --volume="$PWD/_site:/srv/jekyll/_site" \
+        --volume="$PWD/_bundle:/usr/local/bundle" \
+        jekyll/jekyll:$JEKYLL_VERSION \
+        bundle update
 }
 
-case $1 in 
+run() {
+   
+    docker run --rm \
+        --env JEKYLL_UID=$UID \
+        --env JEKYLL_GID=$UID \
+        --volume="$PWD:/srv/jekyll" \
+        --volume="$PWD/_site:/srv/jekyll/_site" \
+        --volume="$PWD/_bundle:/usr/local/bundle" \
+        --publish 4000:4000 \
+        --publish 35729:35729 \
+        jekyll/jekyll:$JEKYLL_VERSION \
+        jekyll serve --livereload --incremental
+}
+
+case $1 in
+    update)
+        update;;
     run)
         run
     ;;
     *)
-        echo "Usage: $0 init"
+        echo "Usage: $0 run | update"
     ;;
 esac
